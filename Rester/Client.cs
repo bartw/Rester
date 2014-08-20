bdfe8a24347439899d5b98aa99cad59b8895e3cd
bartw <bartwijnants@outlook.com>
2014-08-20 20:05:08 +0200

    Creation

---
+++ Rester/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BeeWee.Rester
+{
+    public class Client
+    {
+        public async Task<HttpResponseMessage> Get(string uri)
+        {
+            return await Get(uri, null);
+        }
+
+        public async Task<HttpResponseMessage> Get(string uri, Dictionary<string, string> headers)
+        {
+            var client = new HttpClient();
+            var request = CreateRequest(new Uri(uri), HttpMethod.Get, headers);
+            return await client.SendAsync(request);
+        }
+
+        public async Task<HttpResponseMessage> Get(string uri, Dictionary<string, string> headers, string consumerKey, string consumerSecret)
+        {
+            return await Get(uri, headers, consumerKey, consumerSecret, null, null, null);
+        }
+
+        public async Task<HttpResponseMessage> Get(string uri, Dictionary<string, string> headers, string consumerKey, string consumerSecret, string oauthKey, string oauthSecret)
+        {
+            return await Get(uri, headers, consumerKey, consumerSecret, oauthKey, oauthSecret, null);
+        }
+
+        public async Task<HttpResponseMessage> Get(string uri, Dictionary<string, string> headers, string consumerKey, string consumerSecret, string oauthKey, string oauthSecret, string verifier)
+        {
+            var method = HttpMethod.Get;
+
+            var oauthHeaders = OAuthHelper.GenerateOAuthHeaders(uri, method.ToString(), consumerKey, consumerSecret, oauthKey, oauthSecret, verifier);
+
+            if (headers == null)
+            {
+                headers = oauthHeaders;
+            }
+            else
+            {
+                headers = Utils.Merge(new Dictionary<string, string>[] { headers, oauthHeaders });
+            }
+
+            var client = new HttpClient();
+            var request = CreateRequest(new Uri(uri), method, headers);
+            return await client.SendAsync(request);
+        }
+
+        private HttpRequestMessage CreateRequest(Uri uri, HttpMethod method, Dictionary<string, string> headers)
+        {
+            var request = new HttpRequestMessage(method, uri);
+
+            if (headers != null && headers.Count > 0)
+            {
+                foreach (var header in headers)
+                {
+                    request.Headers.Add(header.Key, header.Value);
+                }
+            }
+
+            return request;
+        }
+    }
+}
---
+++ Rester/OAuthHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace BeeWee.Rester
+{
+    internal static class OAuthHelper
+    {
+        public static Dictionary<string, string> GenerateOAuthHeaders(string uri, string method, string consumerKey, string consumerSecret, string tokenKey, string tokenSecret, string verifier)
+        {
+            var random = new Random();
+            var headers = new Dictionary<string, string>();
+
+            headers.Add("oauth_consumer_key", consumerKey);
+            headers.Add("oauth_nonce", random.Next().ToString());
+            headers.Add("oauth_timestamp", DateTime.UtcNow.ToUnixTime().ToString());
+            headers.Add("oauth_signature_method", "HMAC-SHA1");
+            headers.Add("oauth_version", "1.0");
+
+            if (tokenKey != null && tokenKey.Length > 0)
+            {
+                headers.Add("oauth_token", tokenKey);
+            }
+
+            if (verifier != null && verifier.Length > 0)
+            {
+                headers.Add("oauth_verifier", verifier);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("OAuth ");
+
+            foreach (var header in headers)
+            {
+                sb.Append(string.Format("{0}=\"{1}\",", header.Key, header.Value));
+            }
+
+            sb.Append(string.Format("oauth_signature=\"{0}\"", GenerateSignature(uri, method, consumerSecret, tokenKey, tokenSecret, headers)));
+
+            headers.Add(HttpRequestHeader.Authorization.ToString(), sb.ToString());
+
+            return headers;
+        }
+
+        private static string GenerateSignature(string uri, string method, string consumerSecret, string tokenKey, string tokenSecret, Dictionary<string, string> headers)
+        {
+            var hmacKeyBase = consumerSecret.UrlEncode() + "&" + ((tokenSecret == null) ? "" : tokenSecret).UrlEncode();
+
+            var orderedHeaders = headers.OrderBy(p => p.Key).ThenBy(p => p.Value);
+
+            StringBuilder stringParameter = new StringBuilder();
+            foreach (var header in orderedHeaders)
+            {
+                stringParameter.Append(string.Format("{0}={1}&", header.Key.UrlEncode(), header.Value.UrlEncode()));
+            }
+
+            stringParameter.Remove(stringParameter.Length - 1, 1);
+
+            string signatureBase = string.Format("{0}&{1}&{2}",
+                method.ToUpper(),
+                new Uri(uri).GetComponents(UriComponents.SchemeAndServer | UriComponents.Path, UriFormat.Unescaped).UrlEncode(),
+                stringParameter.ToString().UrlEncode());
+
+            var hash = HmacSha1Sign(Encoding.UTF8.GetBytes(hmacKeyBase), signatureBase);
+
+            return Convert.ToBase64String(hash).UrlEncode();
+        }
+
+        private static byte[] HmacSha1Sign(byte[] keyBytes, string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            MacAlgorithmProvider objMacProv = MacAlgorithmProvider.OpenAlgorithm("HMAC_SHA1");
+            CryptographicKey hmacKey = objMacProv.CreateKey(keyBytes.AsBuffer());
+            IBuffer buffHMAC = CryptographicEngine.Sign(hmacKey, messageBytes.AsBuffer());
+            return buffHMAC.ToArray();
+
+        }
+    }
+}
