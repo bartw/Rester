5c6a378a9dfc52073d18c34e07a82bf942fdfcb1
bartw <bartwijnants@outlook.com>
2014-11-21 20:18:11 +0100

    Removed dependency

---
+++ Rester/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BeeWee.Rester
+{
+    public static class Extensions
+    {
+        private static readonly DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ToUnixTime(this DateTime target)
+        {
+            return (long)(target - unixEpoch).TotalSeconds;
+        }
+
+        public static string UrlEncode(this string stringToEscape)
+        {
+            return Uri.EscapeDataString(stringToEscape)
+                .Replace(" ", "+")
+                .Replace("!", "%21")
+                .Replace("*", "%2A")
+                .Replace("'", "%27")
+                .Replace("(", "%28")
+                .Replace(")", "%29");
+        }
+    }
+}
