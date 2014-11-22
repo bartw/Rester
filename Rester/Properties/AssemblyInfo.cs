458715e8c4b06bc9089a64af86727beb2838feb9
bartw <bartwijnants@outlook.com>
2014-11-22 17:46:07 +0100

    meta

--- Rester/Properties/AssemblyInfo.cs
+++ Rester/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rester")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rester is a Windows 8.1 and Windows Phone 8.1 library for talking to restful api's")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BeeWee")]
 [assembly: AssemblyProduct("Rester")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 BeeWee")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
